10577f840c6e391b9e7b19063a14fed3da2f6453
Greengreener <36747370+Greengreener@users.noreply.github.com>
2020-03-18 12:29:41 +1100

    Initial commit

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform currentPos;
+    public Transform playerPos;
+    public new Vector3 currentVec;
+    public float cameraSpeed = 10f;
+
+    private void Start()
+    {
+        currentVec = currentPos.position;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(currentPos.position, playerPos.position, 5f * Time.fixedDeltaTime);
+        if (Vector3.Distance(currentPos.position, playerPos.position) >= 3 )
+       {
+            camMovement();
+       }
+       else
+       {
+            //camMovement();
+       }
+    }
+    public void camMovement()
+    {
+    }
+}
---
+++ Assets/Scripts/Player1Movement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace player1
+{
+    public class Player1Movement : MonoBehaviour
+    {
+        public Rigidbody2D rb;
+
+        public int speed = 500;
+        public int jumpSpeed = 10000;
+
+        public int jumps = 2;
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        public void Update()
+        {
+            if (jumps > 0)
+            {
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    rb.velocity = new Vector2(0, 1) * jumpSpeed;
+                    jumps--;
+                }
+            }
+
+        }
+
+        public void FixedUpdate()
+        {
+            //Horizontal movement
+            transform.Translate(new Vector3(Input.GetAxis("Horizontal"),0,0) * speed * Time.deltaTime);
+            //Old movement
+            /*float moveHorizontal = Input.GetAxis("Horizontal");
+            Vector3 movement = new Vector3(moveHorizontal, 0, 0);
+            transform.Translate(movement * speed * Time.deltaTime);*/
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb.velocity = new Vector2(0, -1) * 10;
+            }
+
+            /*    if (jumps > 0)
+            {
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    rb.velocity = new Vector2(0, 1) * jumpSpeed;
+                    jumps--;
+                }
+            }*/
+        }
+
+        private void OnTriggerEnter2D(Collider2D Grounded)
+        {
+            if (Grounded.gameObject.tag == "Ground")
+            {
+                jumps = 2;
+            }
+        }
+        private void OnTriggerExit2D(Collider2D Air)
+        {
+            if (Air.gameObject.tag == "Ground")
+            {
+                jumps = 1;
+            }
+        }
+
+    }
+}
