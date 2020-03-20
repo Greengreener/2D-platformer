fa577a59f787b38070e0d2174c966d488c523b65
Greengreener <36747370+Greengreener@users.noreply.github.com>
2020-03-20 12:31:43 +1100

    Fixed dying and respawn add

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -6,16 +6,21 @@ public class CameraControl : MonoBehaviour
 {
     public Transform currentPos;
     public Transform playerPos;
-    public new Vector3 currentVec;
+    public Vector3 currentVec;
     public float cameraSpeed = 10f;
 
     private void Start()
     {
         currentVec = currentPos.position;
     }
+    private void Update()
+    {
 
+    }
     void FixedUpdate()
     {
+        currentPos.position = new Vector3(Mathf.Clamp(currentPos.position.x, playerPos.position.x - 10, playerPos.position.x + 10), Mathf.Clamp(currentPos.position.y, playerPos.position.y - 2, playerPos.position.y + 2), 0);
         transform.position = Vector3.Lerp(currentPos.position, playerPos.position, 5f * Time.fixedDeltaTime);
+
     }
 }
