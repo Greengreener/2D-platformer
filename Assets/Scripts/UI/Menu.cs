bfdc4c65b806040bad7751df9b78dd2d6a436d59
Mesmaa <60990360+Julianm-25@users.noreply.github.com>
2020-04-01 14:30:05 +1100

    1/4

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ChangeScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
